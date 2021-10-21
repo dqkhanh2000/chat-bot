3254f5649fb099f3d5612be9a7990b477b9a208b
Khanh Duong <dqkhanh.2k@gmail.com>
2021-10-21 16:50:40 +0700

    - remove unused file

--- src/pre_process.py
+++ src/pre_process.py
@@ -16,6 +16,13 @@ def split_to_sentences(document):
         result.extend(split_sent_to_word(sentence))
     return result
 
+def loading_stopWord():
+    stopWord = []    
+    with open('./Data/stopWord.txt', 'r', encoding='utf-8') as f:
+        for s_line in f:
+            stopWord.append(s_line.strip())
+    return stopWord
+
 if __name__ == "__main__":
   file = open("demo-full.txt", "r")
   data = file.readlines()
