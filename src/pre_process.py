af211e3712196e54beb1023209ac99fe0e24590c
Khanh Duong <dqkhanh.2k@gmail.com>
2021-10-22 15:22:34 +0700

    * improve preprocess

--- src/pre_process.py
+++ src/pre_process.py
@@ -3,6 +3,9 @@ from underthesea  import word_tokenize
 import re
 import time
 from langdetect import detect
+import threading
+import math
+import io
 
 path = "./"
 
@@ -13,7 +16,7 @@ def split_sent_to_word(sentence, stop_word):
         word = word.strip()
         if (" " not in word and len(word) > 7) or (len(word) == 0):
                 continue
-        if word not in stop_word and detect(word) == 'vi':
+        if word not in stop_word:
             
             words.append(word.lower())
     return words
@@ -25,7 +28,7 @@ def create_vocab(document, stop_word, start_line = 0):
     print("OPENING OUTPUT FILE")
     file_index = -1
     file_name = path + "/output/word_" + str(file_index) + ".txt"
-    stop = 500
+    stop = 10000
 
     output = False
 
@@ -69,19 +72,74 @@ def loading_stopWord():
             stopWord.append(s_line.strip())
     return stopWord
 
+def run_in_thread(start_line, end_line, path, file_name, document, stop_word):
+    # output_data = open(path+"/output/"+file_name+".txt", "wb")
+    output_info = open(path+"/thread_info/"+file_name+"info.txt", "w")
+    output_data = io.FileIO(path+"/output/"+file_name+".txt", 'w')
+    writer = io.BufferedWriter(output_data,buffer_size=100000000)
+
+    print(f"START PROCESS {file_name}............")
+    start_time = time.time()
+
+    for line in range(start_line, end_line+1):
+        if line < len(document):
+            sentence = document[line]
+            
+            output_info.write(f"Processing line {line}\n")
+
+            sentence = sentence.lower()
+            words = split_sent_to_word(sentence, stop_word)
+            if(len(words) == 0):
+                continue
+            s = json.dumps(words, ensure_ascii=False)
+            writer.write(f"{s}\n")
+            writer.flush()
+
+    end_time = time.time()
+    duration = end_time - start_time
+    print(f"write to {file_name} in {duration} seconds")
+    writer.close()
+    output_data.close()
+    output_info.close()
 
 if __name__ == "__main__":
 
     start_time = time.time()
     print("OPENING DATA FILE...")
-    file = open("./data/demo-full.txt", "r")
+    file = open("./data/a.txt", "r")
     print("READING DATA FILE...")
     data = file.readlines()
     end_time = time.time()
     duration = end_time - start_time
     print(f"READ DATA DONE (in {duration})")
 
+    start_time = end_time
     stop_word = loading_stopWord()
-    words = create_vocab(data, stop_word, 342)
+    # words = create_vocab(data, stop_word)
+    # end_time = time.time()
+    # duration = end_time - start_time
+    # print(f"DONE (in {duration})")
+
+    threads = []
+    total_line = len(data)
+    line_in_file = 10000
+    total_file = math.floor(total_line/line_in_file) +1
+    print(total_file)
+
+    for index in range(0, total_file):
+        start_line = index * line_in_file
+        end_line = start_line + line_in_file
+        file_name = f"word_{index}"
+        t = threading.Thread(target=run_in_thread, args=(start_line, end_line, path, file_name, data, stop_word, ))
+        t.start()
+        threads.append(t)
+
+    for thread in threads:
+        thread.join()
+
+
+    end_time = time.time()
+    duration = end_time - start_time
+    print(f"DONE (in {duration})")
 
  
